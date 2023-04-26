3eaa3db6d7b5bebee201824c52ac151b0e929287
NIKAKADE <NIKAKADE@LIN24008241>
2023-04-26 16:25:43 +0530

    Example

---
+++ OverridingExample/overriding/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace overriding
+{
+    public class BaseClass
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("Base class method");
+        }
+    }
+    public class DerivedClass: BaseClass
+    {
+        public override void print()
+        {
+            Console.WriteLine("Derived class method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BaseClass B = new DerivedClass();
+            B.print();
+        }
+    }
+}
